3b4afbcefc77c72bf0a9ba87b8da175e2400b7dd
Masood Arif <masoodarif1313@gmail.com>
2021-10-14 19:27:26 +0500

    Add project files.

---
+++ FYP1/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+       
+    }
+}
