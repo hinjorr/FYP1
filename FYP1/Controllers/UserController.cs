730e5dce7aa017b29328393b974cd491bd57fc11
Masood Arif <masoodarif1313@gmail.com>
2021-10-16 21:26:37 +0500

    9 26 10/16 masood

---
+++ FYP1/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FYP1.Controllers
+{
+    public class UserController : Controller
+    {
+     
+        public IActionResult AddNewUser()
+        {
+            return View();
+        }
+        public IActionResult ViewUsers()
+        {
+            return View();
+        }
+          public IActionResult UploadBulkUsers()
+        {
+            return View();
+        }
+       
+    }
+}
