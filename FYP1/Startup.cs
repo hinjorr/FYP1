9470393972f2ac624e76ce18c509d46e2e312176
Masood Arif <masoodarif1313@gmail.com>
2021-10-19 13:17:25 +0500

    Update Startup.cs

--- FYP1/Startup.cs
+++ FYP1/Startup.cs
@@ -51,7 +51,7 @@ namespace FYP1
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-                //Pull
+                //Pull osama
             });
         }
     }
