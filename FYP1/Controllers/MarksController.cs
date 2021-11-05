acd6f05e65ae770e216a68969b75c6284cd4442f
Abdul Basit Khan <75264503+abdulbasitlogicstack@users.noreply.github.com>
2021-11-06 02:22:55 +0500

    new work

---
+++ FYP1/Controllers/MarksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP1.Controllers
+{
+    public class MarksController : Controller
+    {
+        
+        public IActionResult UploadResults()
+        {
+            return View();
+        }
+    }
+}
---
+++ FYP1/wwwroot/LMS/ViewsJquery/UploadResults.js
@@ -0,0 +1,40 @@
+﻿
+$.validator.addMethod('ge', function (value, element, param) {
+    return this.optional(element) || value >= $(param).val();
+}, 'Invalid value');
+
+$("#Upload").validate({
+    rules: {
+        totalMarks: {
+            required: true,
+            digits: true,
+        },
+        obtainedmarks: {
+            required: true,
+            digits: true,
+            ge: '#totalMarks',
+        },
+
+    },
+    messages: {
+        totalMarks: {
+            required: "Please enter Total Marks",
+            digits: "Please enter Digits",
+        },
+        obtainedmarks: {
+            required: "Please enter Obtained Marks",
+            digits: "Please enter Digits",
+            ge: 'Must be greater than or equal to Total Marks',
+        },
+    },
+    submitHandler: function (form) {
+        
+        var UploadRes = {
+                TotalMarks : $("#txtTotalMarks").val,
+                ObtainedMarks : $("#txtObtainedMarks").val,
+        };
+        console.log(UploadRes);
+
+    }
+
+})
