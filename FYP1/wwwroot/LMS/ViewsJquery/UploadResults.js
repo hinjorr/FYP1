b9cd36e637d89a9597fef295fb986673012860b0
Masood Arif <masoodarif1313@gmail.com>
2021-11-06 18:24:07 +0500

    Add files via upload

---
+++ FYP1/wwwroot/LMS/ViewsJquery/UploadResults.js
@@ -0,0 +1,40 @@
+﻿$.validator.addMethod("greaterThan",
+    function (value, element, param) {
+        var $otherElement = $(param);
+        return parseInt(value, 10) >= parseInt($otherElement.val(), 10);
+    });
+$("#Upload").validate({
+    rules: {
+        totalMarks: {
+            required: true,
+            digits: true,
+        },
+        obtainedmarks: {
+            required: true,
+            digits: true,
+            greaterThan: "#txtTotalMarks",
+        },
+
+    },
+    messages: {
+        totalMarks: {
+            required: "Please enter Total Marks",
+            digits: "Please enter Digits",
+        },
+        obtainedmarks: {
+            required: "Please enter Obtained Marks",
+            digits: "Please enter Digits",
+            greaterThan: 'Must be greater than or equal to Total Marks',
+        },
+    },
+    submitHandler: function (form) {
+        
+        var UploadRes = {
+                TotalMarks : $("#txtTotalMarks").val,
+                ObtainedMarks : $("#txtObtainedMarks").val,
+        };
+        console.log(UploadRes);
+
+    }
+
+})
